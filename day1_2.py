33dc16bd24e02d8a87210ce31b11c932de729d62
robert96028 <robert96028@gchq.gov.uk>
2021-12-06 17:51:49 +0000

    improvements to day 1, pt 2

--- day1_2.py
+++ day1_2.py
@@ -1,4 +1,18 @@
+import sys
+
 filename = 'input1'
+
+
+def sumWindow(measurementList):
+    print("in sumWindow")
+    total = 0
+    for measurement in measurementList:
+        total = total + measurement
+    print("total = {}".format(total))
+
+# def compareWindows(window, measurementIndex):
+#     # create next time window list
+
 
 with open(filename) as f:
     lines = f.readlines()
@@ -9,7 +23,7 @@ tup = tuple(lines)
 halt_early = True
 halt_index = 10
 
-print(type(tup))
+# print(type(tup))
 # capture measurements in a list
 # clear group out when we've processed a block of 3
 # find window of 3 using modulus
@@ -17,33 +31,33 @@ print(type(tup))
 # dict usage dictname['key] or dictname = {'key1':value, 'key2':value}
 # append to list = listname.append(value)
 increase = 0
-trioList = [];
+measurements = [];
+measurementDict = {};
+numWindows = 0;
+
+#  need to compare 3 numbers (a) with next 3 numbers (b), with pos incr of 1 for b
+
+
+# sys.exit(0)
 
 for measurement_index in range(1, len(tup)):
 
-    # print(measurement_index)
-    next_measurement = int(tup[measurement_index])
-
-    trioList.append(int(tup[measurement_index-1])) 
-
+    # we've reached a multiple of 3
     if (measurement_index % 3 == 0):
         print("found end of window at pos {}".format(measurement_index))
-        # capture list and reset
-        print(trioList)
-        trioList = [];
+        trioList = [i for i in range(measurement_index-3, measurement_index)]
+        print("window list = {} ".format(trioList))
+        measurementDict[len(measurementDict)] = trioList
 
-
-
-    next_measurement = int(tup[measurement_index])
-    curr_measurement = int(tup[measurement_index-1])
-    # print(type(curr_measurement))
-    # print("is {0} > {1}".format(curr_measurement, next_measurement))
-    if (next_measurement > curr_measurement):
-        print("increase")
-        increase = increase +1 
 
     if (halt_early and measurement_index == halt_index):
         print("halted at {} ".format(measurement_index))
         break
 
 print("increased measurements = {0}".format(increase))
+
+for i in range (len(measurementDict)):
+    print("dictionary window list = {}".format(measurementDict[i]))
+
+
+
