d6c4932851ac7288591da7b1beec80fd06d5c8d4
robert96028 <robert96028@gchq.gov.uk>
2022-01-07 19:21:57 +0000

    completed day2

--- day2/day2_2.py
+++ day2/day2_2.py
@@ -5,7 +5,7 @@ sys.path.append('../')
 
 import read_file
 
-tup = read_file.get_file_contents('sample_input')
+tup = read_file.get_file_contents('input')
 # print(len(tup))
 # print(type(tup))
 # print(tup)
@@ -33,30 +33,38 @@ def convert_list_to_dict(list_arg):
 
 
 def identify_instruction(instruction):
-
+    print("instruction = {}".format(instruction))
     global horiz_pos, aim, depth
 
     if instruction[0] == 'forward':
-        process_forward(horiz_pos, depth, aim, instruction[1])
+        depth, horiz_pos = process_forward(horiz_pos, depth, aim, instruction[1])
+        print("depth = {}, horizontal pos = {}".format(depth, horiz_pos))
     else:
         aim =  aim + adjust_aim(instruction)
         print("aim = {}".format(aim))
 
+# down X increases your aim by X units.
+# up X decreases your aim by X units.
+# forward X does two things:
+# It increases your horizontal position by X units.
+# It increases your depth by your aim multiplied by X.
+
 
 def adjust_aim(reading_input):
     print("direction is {}, aim adjust is {}".format(reading_input[0], reading_input[1]))
-    if reading_input[0] == 'down':
+    if reading_input[0] == 'up':
         return - int(reading_input[1])
-    # must be up
+    # down
     return int(reading_input[1])
 
 # TODO complete forward calculations
 def process_forward(horizontal_pos, depth, aim, reading):
+
+    # global depth, horizontal_pos
     print("forward distance = {}".format(reading))
     horizontal_pos = horizontal_pos + int(reading)
-    depth = (depth * aim) * reading
-    print("depth = {}".format(depth))
-    return depth
+    depth = (aim * int(reading)) + depth
+    return depth, horizontal_pos
 
 readings_list = split_instruction_out(tup)
 print(readings_list)
@@ -64,8 +72,11 @@ print(readings_list)
 count = 0
 for reading in readings_list:
     identify_instruction(reading)
-    count = count + 1
-    if count == 10:
-        break
+    # count = count + 1
+    # if count == 10:
+    #     break
 
-print("aim = {}, depth = {}".format(aim, depth))
+
+
+print("aim {}, depth {}, horizontal pos {}".format(aim, depth, horiz_pos))
+print("final pos {}".format(horiz_pos * depth))
